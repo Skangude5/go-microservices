19ea6e896433b0b7e909cd6ab29f224577b0ceb8
Sharad Kangude <skangude5@icloud.com>
2023-10-03 18:40:46 +0530

    added benchmark test

---
+++ mvc/domain/user_doa_test.go
@@ -0,0 +1,27 @@
+package domain
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestGetUserNoUserFound(t *testing.T) {
+	user, err := GetUser(0)
+
+	assert.Nil(t, user, "we were not expecting a user with id 0")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "user 0 doest not exists", err.Message)
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+
+	assert.EqualValues(t, 123, user.Id)
+
+}
